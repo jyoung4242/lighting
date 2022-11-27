869da9cd205c4b22daefffd6943c350c43e7d078
Justin Young <justin_dean_young@yahoo.com>
2022-11-27 10:55:07 -0500

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import "./style.css";
 import { UI } from "@peasy-lib/peasy-ui";
 import { Lighting, Vector, Light } from "@peasy-lib/peasy-lighting";
-import { version } from "html-webpack-plugin";
+import playerNormal from "./assets/playernormal.png";
+import roidNormal from "./assets/asteroid normal.png";
 
 const template = `
 <div class="content">
-    <div id="\${ship.id}" class="ship" style="translate: \${ship.position.x}px \${ship.position.y}px; rotate: \${ship.orientation}deg"></div>
-    <div id="\${roid.id}" class="asteroid" style="translate: \${roid.position.x}px \${roid.position.y}px; rotate: \${roid.orientation}deg background-position: \${roid.cssPosition}"></div>
+    <div id="\${ship.id}" class="ship" style="translate: \${ship.position.x}px \${ship.position.y}px;transform-origin: center; rotate: \${ship.orientation}deg"></div>
+    <div id="\${roid.id}" class="asteroid" style="translate: \${roid.position.x}px \${roid.position.y}px; rotate: \${roid.orientation}deg; transform-origin: center; background-position: \${roid.cssPosition}"></div>
 </div>
 `;
 
@@ -17,6 +18,7 @@ const model = {
     position: new Vector(100, 10),
     orientation: 0,
     entity: undefined as any,
+    sizeoffset: new Vector(25, 25),
   },
   roid: {
     id: "roid",
@@ -25,6 +27,7 @@ const model = {
     orientation: 0,
     entity: undefined as any,
     cssPosition: "0px 0px",
+    sizeoffset: new Vector(40, 40),
   },
 };
 let viewport: HTMLElement;
@@ -46,24 +49,26 @@ let light = Lighting.addLight({
 model.ship.entity = Lighting.addEntities([
   {
     id: "ship",
-    position: model.ship.position,
+    position: model.ship.position.add(model.ship.sizeoffset),
     orientation: model.ship.orientation,
     size: new Vector(50, 50),
-    normalMap: "./assets/playernormal.png",
+    normalMap: playerNormal,
   },
 ]);
 
 model.roid.entity = Lighting.addEntities([
   {
     id: "roid",
-    position: model.roid.position,
+    position: model.roid.position.add(model.roid.sizeoffset),
     orientation: model.roid.orientation,
     size: new Vector(80, 80),
-    normalMap: "./assets/asteroid normal.png",
+    normalMap: roidNormal,
   },
 ]);
 
 setInterval(() => {
+  model.ship.orientation += 0.5;
+  model.roid.orientation += 0.5;
   UI.update();
   Lighting.update();
 }, 1000 / 60);
